2814d1dfec5061df12f1d840f14bf659d99cde17
Logunathan J <logulogu20032003@gmail.com>
2024-08-16 20:19:28 +0530

    API Integration

--- CRUD.Service/main.py
+++ CRUD.Service/main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+data_list = ["Logu","Gowtham","Kavi","Kishore","KRP"]
+
+@app.route('/add', methods=['POST'])
+def add_data():
+    print("add_data")
+    new_data = request.json
+    print("after",new_data)
+    if new_data:
+        data_list.append(new_data['name'].capitalize())
+        return jsonify({"message": "Data added successfully"}), 201
+    else:
+        return jsonify({"error": "No data provided"}), 400
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify(data_list),201
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- CRUD.UI/frontend/src/app/app.component.ts
+++ CRUD.UI/frontend/src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule,CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  list_data:any;
+  input_data = new FormGroup({
+    name: new FormControl("")
+  })
+
+  constructor(private http:HttpClient){  }
+
+  ngOnInit(): void {
+    this.getAllData();
+    // console.log("worked",this.list_data);
+  }
+
+  getAllData(){
+    this.http.get<any>("http://127.0.0.1:5000/data").subscribe(data => {
+      this.list_data=data;
+      // console.log("get",this.list_data);
+    },error => {
+      // console.error('Error occurred:', error);
+    });
+  }
+
+  addNewData(e:any){
+    e.preventDefault();
+    var msg:any;
+    this.http.post("http://127.0.0.1:5000/add",{name:this.input_data.controls.name.value}).subscribe(data => {
+      msg = data
+      // console.log("post",msg);
+      this.getAllData();
+    });
+    this.input_data.controls.name.setValue("");
+
+  }
+
 }
